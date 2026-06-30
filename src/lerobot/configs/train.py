a960958d41edb7d56ac7f8a71cb7b291d3f963aa
YukiTakesue <yuuki.takesue@techshare.co.jp>
2026-06-30 19:38:12 +0900

    modified config/train.py. push to hub

--- src/lerobot/configs/train.py
+++ src/lerobot/configs/train.py
@@ -139,9 +139,7 @@ class TrainPipelineConfig(HubMixin):
             self.scheduler = self.policy.get_scheduler_preset()
 
         if self.policy.push_to_hub and not self.policy.repo_id:
-            raise ValueError(
-                "'policy.repo_id' argument missing. Please specify it to push the model to the hub."
-            )
+            self.policy.push_to_hub = False
 
         if self.use_rabc and not self.rabc_progress_path:
             # Auto-detect from dataset path
